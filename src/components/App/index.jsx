0bb9f5a399fef85b51981cfc322bdc1e64260ca3
BharathiRaja <bharathiraja@frontdesk.ai>
2018-09-23 15:50:54 -0700

    Including App component

---
+++ src/components/App/index.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+import './index.scss';
+
+
+
+class App extends Component {
+	render() {
+return (
+		<div className="container"> 
+		<h1 className="container__header">My React App</h1>  
+		</div> 
+	   );
+	}
+}
+
+export default App;
